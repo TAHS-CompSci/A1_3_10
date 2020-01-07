cf4068ba5c2d4805d37380e5f4b46bd8d0ded20f
GC3018 <57916732+GC3018@users.noreply.github.com>
2020-01-07 13:20:38 -0600

    1.3.10 Campbell
    update on prisiners dilema codse

--- team0.py
+++ team0.py
@@ -15,7 +15,7 @@ def move(my_history, their_history, my_score, their_score):
     if len(my_history)==0:
         return 'c'
     else
-        return (their_history) input()
+        return (their_history)[-1] input()
             if 'b'
                 return 'b'
             else
