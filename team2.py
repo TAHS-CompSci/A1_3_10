3fda4ac23d012300b22c5b9e011be81f3a135b5e
Drayzdin <57916828+Dt3202@users.noreply.github.com>
2020-02-06 13:10:07 -0600

    New Code

--- team2.py
+++ team2.py
@@ -6,56 +6,41 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Cryosthenos' # Only 10 chars displayed.
-strategy_name = 'Sneaky betrayal'
-strategy_description = 'Betray and trick the other team to collude'
+team_name = 'Drayzdin' # Only 10 chars displayed.
+strategy_name = 'Simple Perception'
+strategy_description = 'Collude or Betray based off of how it went with pevious rounds'
 points = []
 my_history2 = []
 their_history2 = []
 
 def move(my_history, their_history, my_score, their_score):
-	global points
-	global my_history2
-	global their_history2
-	their_history2 += their_history
-	my_history2 += my_history
-	while len(my_history2) > 2:
-		if my_history2[-1] == their_history2[-1] and my_history2[-1] == 'c':
-			points += [0]
-		if my_history2[-1] == their_history2[-1] and my_history2[-1] == 'b':
-			points += [-250]
-		if my_history2[-1] != their_history2[-1] and my_history2[-1] == 'c':
-			points += [-500]
-		if my_history2[-1] != their_history2[-1] and my_history2[-1] == 'b':
-			points += [100]
-		if len(points) >= 2: 
-			if points[-1] == points[-2]:
-				return their_history2[-2]
-			if points[-1] < points[-2]:
-				return their_history2[-1]
-			if points[-1] > points[-2]:
-				return my_history2[-1]
-			else:
-				return 'b'
-	else:
-		return 'b'
+    global points
+    global my_history2
+    global their_history2
+    their_history2 += their_history
+    my_history2 += my_history
+    while len(my_history2) > 2:
+        if my_history2[-1] == their_history2[-1] and my_history2[-1] == 'c':
+            points += [0]
+        if my_history2[-1] == their_history2[-1] and my_history2[-1] == 'b':
+            points += [-250]
+        if my_history2[-1] != their_history2[-1] and my_history2[-1] == 'c':
+            points += [-500]
+        if my_history2[-1] != their_history2[-1] and my_history2[-1] == 'b':
+            points += [100]
+        if len(points) >= 2: 
+            if points[-1] == points[-2]:
+                return their_history2[-2]
+            if points[-1] < points[-2]:
+                return their_history2[-1]
+            if points[-1] > points[-2]:
+                return my_history2[-1]
+            else:
+                return 'b'
+    else:
+        return 'b'
    
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-
-    
+   
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
